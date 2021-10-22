2dbc3281d2f49af7b4e1c3f245abaa51440ae65b
VengerAlex <alexvenger90@gmail.com>
2021-10-22 20:56:38 +0300

    did clickHandler func thus by clicking on each country we are able to download info

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import {Switch, Route} from "react-router-dom";
+
 import Header from "./components/Header";
-import {Main} from "./components/Main";
-import Controls from "./components/Controls";
+import {Main} from "./components/Main"
+import HomePage from "./pages/HomePage";
+import NotFound from "./pages/NotFound";
+import Details from "./pages/Details";
 
 const App = (props) => {
+
     return (
         <>
             <Header/>
             <Main>
-                <Controls />
+                <Switch>
+                    <Route exact path='/' component={HomePage} />
+                    <Route exact path='/country/:name' component={Details} />
+                    <Route path='/' component={NotFound} />
+                </Switch>
             </Main>
         </>
     )
